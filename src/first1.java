6411fa778d062a38a5e12d841b68b2c0b9b86bba
ricky80022 <105021088@live.asia.edu.tw>
2016-09-21 12:15:32 +0800

    week01

---
+++ src/first1.java
@@ -0,0 +1,9 @@
+
+public class first1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String nickName = "資工人";
+String uni = "亞洲大學";
+System.out.println(nickName);
+System.out.print(uni + "\n");
+System.out.println(nickName + "  " + uni);
+	}
+
+}
